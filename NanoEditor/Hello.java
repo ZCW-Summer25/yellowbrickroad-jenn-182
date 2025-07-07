e1e8bdea80cc80174ae3fa556d550ed444c0837f
jenn-182 <jenniferstaci182@gmail.com>
2025-07-07 17:22:28 -0400

    Hello Java work, finsihed

--- NanoEditor/Hello.java
+++ NanoEditor/Hello.java
@@ -1,7 +1,6 @@
-'''java
 public class Hello {
-public static void main(String[]args) {
-System.out.println("Hello, Java!");
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("Hello, again!");
+    }
 }
-}
-'''
